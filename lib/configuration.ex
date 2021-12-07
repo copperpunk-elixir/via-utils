b1f44e362d0e1ae726f9399336e23aa20541f1b7
Greg Gradwell <greghgradwell@live.com>
2021-12-06 21:52:05 -0800

    Need config files to accept value for non-existing key. Needs to be entire value, as we don't know if its a Map or Keyword.

--- lib/configuration.ex
+++ lib/configuration.ex
@@ -2,9 +2,17 @@ defmodule ViaUtils.Configuration do
   require Logger
 
   def merge_configuration_files(default_conf, new_conf) do
-    nested_key_values = get_all_nested_key_values(new_conf)
+    nested_key_values =
+      if is_empty?(new_conf) do
+        []
+      else
+        get_all_nested_key_values(new_conf)
+      end
+
+    Logger.debug("#{__MODULE__} new config: #{inspect(new_conf)}")
 
     Enum.reduce(nested_key_values, default_conf, fn {new_key, new_value}, acc ->
+      Logger.info("new k/v: #{inspect(new_key)}/#{inspect(new_value)}")
       put_in(acc, new_key, new_value)
     end)
   end
@@ -12,15 +20,37 @@ defmodule ViaUtils.Configuration do
   def get_all_nested_key_values(enum, key_values \\ []) do
     nested_key = get_deepest_key_path(enum)
     {nested_value, remaining_enum} = pop_in(enum, nested_key)
+    # Logger.warn("nested value: #{inspect(nested_value)}")
 
-    key_values =
-      if nested_value == [] or nested_value == %{} do
-        key_values
+    key_one_level_up = List.delete_at(nested_key, -1)
+    # Logger.debug("key one up: #{inspect(key_one_level_up)}")
+
+    {remaining_enum, key_values} =
+      if is_empty?(get_value_one_level_up(remaining_enum, key_one_level_up)) do
+        if key_one_level_up == [] do
+          {_, rem_enum} = pop_in(remaining_enum, nested_key)
+          # Logger.debug("KOU empty. Pop nested value")
+          {rem_enum, key_values}
+        else
+          {_, rem_enum} = pop_in(remaining_enum, key_one_level_up)
+          # Logger.debug("v @ kou empty")
+          {rem_enum, [{nested_key, nested_value} | key_values]}
+        end
       else
-        [{nested_key, nested_value} | key_values]
+        {remaining_enum, [{nested_key, nested_value} | key_values]}
       end
 
-    Logger.warn("key_values: #{inspect(key_values)}")
+    # Logger.info("rem enum: #{inspect(remaining_enum)}")
+
+    # key_values = [{nested_key, nested_value} | key_values]
+
+    # if nested_value == [] or nested_value == %{} do
+    #   key_values
+    # else
+    # [{nested_key, nested_value} | key_values]
+    # end
+
+    Logger.debug("key_values: #{inspect(key_values)}")
 
     if Enum.empty?(remaining_enum) do
       key_values
@@ -31,7 +61,7 @@ defmodule ViaUtils.Configuration do
 
   def get_deepest_key_path(enum, path \\ []) do
     first_item = Enum.take(enum, 1)
-    Logger.debug("First item: #{inspect(first_item)}")
+    # Logger.debug("First item: #{inspect(first_item)}")
 
     {first_key, first_value} =
       if is_enum?(first_item) do
@@ -40,21 +70,49 @@ defmodule ViaUtils.Configuration do
         {[], first_item}
       end
 
-    Logger.debug("First value(all): #{inspect(first_value)}")
+    # Logger.debug("First value(all): #{inspect(first_value)}")
     first_value = Enum.at(first_value, 0)
-    Logger.debug("first key/value: #{inspect(first_key)}/#{inspect(first_value)}")
+    # Logger.debug("first key/value: #{inspect(first_key)}/#{inspect(first_value)}")
     path = path ++ first_key
-    Logger.debug("path: #{inspect(path)}")
+    # Logger.debug("path: #{inspect(path)}")
 
     if is_enum?(first_value) do
       get_deepest_key_path(first_value, path)
     else
-      Logger.warn("found path: #{inspect(path)}")
+      # Logger.warn("found path: #{inspect(path)}")
       path
+    end
+  end
+
+  def get_value_one_level_up(enum, key_one_level_up) do
+    if key_one_level_up == [] do
+      # Logger.debug("KOU empty")
+      []
+    else
+      # Logger.debug("v @ KOU: #{inspect(get_in(enum, key_one_level_up))}")
+      get_in(enum, key_one_level_up)
     end
   end
 
   def is_enum?(value) do
     Keyword.keyword?(value) or is_map(value)
+  end
+
+  def is_empty?(value) do
+    value == [] or value == %{}
+  end
+
+  def get_first_matching_key(enum, whole_key) do
+    if is_nil(get_in(enum, whole_key)) do
+      {remaining_key, dropped_key} = Enum.split(whole_key, -1)
+
+      if Enum.empty?(remaining_key) do
+        dropped_key
+      else
+        get_first_matching_key(enum, remaining_key)
+      end
+    else
+      whole_key
+    end
   end
 end
--- test/configuration/get_deepest_key_test.exs
+++ test/configuration/get_deepest_key_test.exs
@@ -23,20 +23,70 @@ defmodule ViaUtils.Configuration.GetDeepestKeyTest do
   test "Update conf" do
     default_conf = [
       a: [x: %{beta: 2, alpha: 1}, y: 4, z: [5]],
-      b: [z: %{foo: 100, bar: -100}, u: 1, v: 2, w: 2 ]
+      b: [z: %{foo: 100, bar: -100}, u: 1, v: 2, w: 2]
     ]
+
+    # default_conf = [
+    #   a: [w: %{b: 1, c: -1}, x: [5], y: 4, z: []]
+    # ]
 
     nested_key_values = ViaUtils.Configuration.get_all_nested_key_values(default_conf)
     Logger.info("nested kvs: #{inspect(nested_key_values)}")
 
     new_conf = [a: [x: %{alpha: 2}, z: []], b: [u: 3, z: %{foo: -200}]]
+    # new_conf = [a: [w: %{b: 0}, x: [1, 2, 3]]]
     merged_conf = ViaUtils.Configuration.merge_configuration_files(default_conf, new_conf)
     Logger.debug("merged conf: #{inspect(merged_conf)}")
     assert get_in(merged_conf, [:a, :x, :alpha]) == 2
     assert get_in(merged_conf, [:b, :u]) == 3
     assert get_in(merged_conf, [:b, :z, :foo]) == -200
     assert get_in(merged_conf, [:b, :v]) == 2
+    assert get_in(merged_conf, [:a, :z]) == []
+    Process.sleep(100)
+  end
 
+  test "Identical configs" do
+    default_conf = [
+      a: [x: %{beta: 2, alpha: 1}, y: 4, z: [5]],
+      b: [z: %{foo: 100, bar: -100}, u: 1, v: 2, w: 2]
+    ]
+
+    new_conf = [
+      a: [x: %{beta: 2, alpha: 1}, y: 4, z: [5]],
+      b: [z: %{foo: 100, bar: -100}, u: 1, v: 2, w: 2]
+    ]
+
+    merged_conf = ViaUtils.Configuration.merge_configuration_files(default_conf, new_conf)
+    Logger.debug("merged conf: #{inspect(merged_conf)}")
+    assert merged_conf == default_conf
+    Process.sleep(100)
+  end
+
+  test "Empty new config" do
+    default_conf = [
+      a: [x: %{beta: 2, alpha: 1}, y: 4, z: [5]],
+      b: [z: %{foo: 100, bar: -100}, u: 1, v: 2, w: 2]
+    ]
+
+    new_conf = []
+
+    merged_conf = ViaUtils.Configuration.merge_configuration_files(default_conf, new_conf)
+    Logger.debug("merged conf: #{inspect(merged_conf)}")
+    assert merged_conf == default_conf
+    Process.sleep(100)
+  end
+
+  test "Put_in value with no existing key " do
+    default_conf = [a: %{x: 0}]
+    new_conf = [a: %{y: 0}, b: [1, 2, 3]]
+
+    matching_key = ViaUtils.Configuration.get_first_matching_key(default_conf, [:b])
+    assert matching_key == [:b]
+
+    merged_conf = ViaUtils.Configuration.merge_configuration_files(default_conf, new_conf)
+    Logger.debug("merged conf: #{inspect(merged_conf)}")
+    assert merged_conf[:a][:y] == 0
+    assert merged_conf[:b] == [1, 2, 3]
     Process.sleep(100)
   end
 end
